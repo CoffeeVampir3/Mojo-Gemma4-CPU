bd0aea8b4c378e1bc6f41eef767fe310024c6f67
Zorgon <coffeevampirebusiness@gmail.com>
2026-06-11 06:38:06 -1000

    Minor support for instruct variant for bf16

--- remote_perf.fish
+++ remote_perf.fish
@@ -106,6 +106,7 @@ rsync -av \
     --exclude='tp_param_bin' \
     --include='checkpoints/SmolLM2/model.safetensors' \
     --include='checkpoints/gemma-4-26B-A4B/*' \
+    --include='checkpoints/gemma-4-26B-A4B-it/*' \
     --include='checkpoints/gemma-4-26B-A4B-bq/model.safetensors' \
     --exclude='checkpoints/**/*.safetensors' \
     . \
